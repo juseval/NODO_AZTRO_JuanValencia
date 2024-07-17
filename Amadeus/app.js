4f4f977f2a68d83832dd4d5abbc19174f4af5731
juan Valencia <juseval98@hotmail.com>
2024-07-17 18:56:38 -0500

    News 17Jul

--- Amadeus/app.js
+++ Amadeus/app.js
@@ -1,11 +1,26 @@
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('.carousel').slick({
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-        });
-    });
-</script>
+const imagenes = ['./Imagenes/Carrusel1.jpg','./Imagenes/Carrusel2.jpg','./Imagenes/Carrusel3.jpg','./Imagenes/Carrusel4.jpg','./Imagenes/Carrusel5.jpg']
+
+fondo = document.querySelectorAll('.imagenes');
+let contador=0;
+
+fondo.forEach(e => {
+  e.style.backgroundImage=`Url(${imagenes[contador]})`;
+  e.style.backgroundRepeat=`noRepeat`;
+  e.style.backgroundSize=`cover`;
+  e.style.backgroundPosition=`center`;
+  contador++
+});
+
+
+const carouselItems = document.querySelectorAll('.carrusel .imagenes');
+
+carouselItems.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    carouselItems.forEach(i => i.classList.remove('mostrar'));
+    item.classList.add('mostrar');
+  });
+
+  item.addEventListener('mouseleave', () => {
+    item.classList.remove('mostrar');
+  });
+});
